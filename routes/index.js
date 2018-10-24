2c1d6d768b79528d27543b5ce8e606d13dc41219
yangqin520620 <l>
2018-10-24 09:22:29 +0800

    更新

--- routes/index.js
+++ routes/index.js
@@ -72,7 +72,11 @@ router.get('/user-manager.html', function(req, res) {
 router.get('/mobile-manager.html', function(req, res) {
   // 同首页，需要判断是否用户登录，并且判断用户是否是管理员
   if (req.cookies.username && parseInt(req.cookies.isAdmin)) {
-    res.render('mobile-manager');
+    res.render('mobile-manager', {
+      username: req.cookies.username,
+      nickname: req.cookies.nickname,
+      isAdmin: parseInt(req.cookies.isAdmin) ? '(管理员)' : '',
+    });
   } else {
     res.redirect('/login.html');
   }
